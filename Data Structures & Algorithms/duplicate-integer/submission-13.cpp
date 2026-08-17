9fa58d2ba5404e8c2fecba941e47144d563e3f92
Devkarpenter <157476541+Devkarpenter@users.noreply.github.com>
2026-08-17 15:38:48 +0530

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+
+        unordered_map<int,int>mpp;
+
+        for(int x : nums){
+            mpp[x]++;
+        }
+
+        for(auto it: mpp){
+            if(it.second >= 2){
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+};
